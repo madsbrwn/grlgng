aa885bc1026b5d3c493978e0e4b97188ad0b1460
Madeline Brown <madsbrwn@gmail.com>
2018-09-28 18:13:44 -0600

    initial commit `

---
+++ Handy Hiker/FirstViewController.swift
@@ -0,0 +1,26 @@
+//
+//  FirstViewController.swift
+//  Handy Hiker
+//
+//  Created by Madeline Brown on 9/24/18.
+//  Copyright © 2018 GrlGng. All rights reserved.
+//
+
+import UIKit
+import FirebaseCore
+import FirebaseDatabase
+
+class FirstViewController: UIViewController {
+
+    var ref : DatabaseReference?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        ref = Database.database().reference()
+    }
+
+
+}
+
---
+++ Handy Hiker/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Handy Hiker
+//
+//  Created by Madeline Brown on 9/24/18.
+//  Copyright © 2018 GrlGng. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
